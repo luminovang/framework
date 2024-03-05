c3fe08d1de58a8402d9f0572e9d3fd8478fbcd27
Chigozie Peter <ujahchigozie@gmail.com>
2024-03-06 03:25:55 +0800

    Update Updater.php

--- src/Composer/Updater.php
+++ src/Composer/Updater.php
@@ -9,6 +9,8 @@
  */
 namespace Luminova\Composer;
 class Updater{
+    private static string $framework = 'luminovang/framework';
+    private static string $frameworkPath = 'system/plugins/luminovang/framework/';
 
     public static function BeforeUpdate(): void 
     {
@@ -20,9 +22,9 @@ class Updater{
         if ($composer !== false) {
             $composer = json_decode($composer, true);
 
-            if (!isset($composer['require']['nanoblocktech/psr-cache'])) {
+            if (!isset($composer['require'][static::$framework])) {
                 echo "Updating Composer.json\n";
-                $composer['require']['nanoblocktech/psr-cache'] = "^1.0";
+                $composer['require'][static::$framework] = "^1.0";
                 $jsonString = json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
                 if ($jsonString !== false) {
                     file_put_contents($composerJsonPath, $jsonString);
@@ -36,34 +38,33 @@ class Updater{
     public static function AfterUpdate(): void 
     {
         if (getenv('LM_DEBUG_MODE') === false) { 
-            static::moveFrameworkDirectory('system/Psr/Cache', 'system/plugins/nanoblocktech/psr-cache/', 'src/');
+            static::onInstallAndUpdateFramework('system/', static::$frameworkPath, 'src/');
         }
     }
 
     public static function InstallFiles(): void 
     {
         if (getenv('LM_DEBUG_MODE') === false) {
-            //self::moveFrameworkDirectory('system/Psr/Cache', 'system/plugins/nanoblocktech/psr-cache/', 'src/');
+            self::onInstallAndUpdateFramework('system/', static::$frameworkPath, 'src/');
             self::checkAndCopyFile('.env', 'samples/.env');
             self::checkAndCopyFile('meta.config.json', 'samples/meta.config.json');
+            self::checkAndCopyFile('class.config.php', 'samples/class.config.php');
             self::checkAndCopyFile('app/Controllers/Config/Session.php', 'samples/Session.php');
             self::checkAndCopyFile('app/Controllers/Config/Cookie.php', 'samples/Cookie.php');
             self::checkAndCopyFile('app/Controllers/Config/Config.php', 'samples/Config.php');
             self::checkAndCopyFile('app/Controllers/Config/Template.php', 'samples/Template.php');
+            self::checkAndCopyFile('app/Controllers/Config/IPConfig.php', 'samples/IPConfig.php');
             self::checkAndCopyFile('app/Controllers/Utils/Global.php', 'samples/Global.php');
             self::checkAndCopyFile('app/Controllers/Utils/Func.php', 'samples/Func.php');
             self::checkAndCopyFile('app/Controllers/Application.php', 'samples/Application.php');
-           // self::checkAndCopyFile('system/autoload.api.php', 'samples/autoload.api.php');
-           // self::checkAndCopyFile('public/index.php', 'samples/index.php');
             self::checkAndCopyFile('public/.htaccess', 'samples/.htaccess');
             self::checkAndCopyFile('public/robots.txt', 'samples/robots.txt');
-           //self::checkAndCopyDirectory('public/api', 'samples/api');
-           // self::makeDirectory('public/assets/');
 
-           
-           self::backwardProjectDirectory();
+            //self::makeDirectory('public/assets/');
+            //self::backwardProjectDirectory();
         }
     }
+
 
     public static function renameProjectRoot(): void {
         $composerJsonPath = __DIR__ . '/../composer.json';
@@ -107,10 +108,19 @@ class Updater{
                         echo "Failed to rename project directory.\n";
                     }
                 } else {
-                    self::checkAndMoveDirectory($projectDestinationDir, $currentProjectDir);
+                    self::checkAndMoveFolderRecursive($projectDestinationDir, $currentProjectDir);
                 }
             }
         }
+    }
+
+    private static function isUpdater(string $dest): bool 
+    {
+        if(in_array($dest, ['Composer/Updater.php', '/Composer/Updater.php', 'system/Composer/Updater.php'])){
+            return true;
+        }
+
+        return false;
     }
 
 
@@ -122,7 +132,7 @@ class Updater{
         }
     }
 
-    private static function checkAndMoveDirectory(string $destination, string $source): void
+    private static function checkAndMoveFolderRecursive(string $destination, string $source): void
     {
         static::makeDirectoryIfNotExist($destination);
         $files = scandir($source);
@@ -130,20 +140,27 @@ class Updater{
             if ($file !== '.' && $file !== '..') {
                 $srcFile = rtrim($source, '/') . "/$file";
                 $dstFile = rtrim($destination, '/') . "/$file";
+                $display = static::displayPath($dstFile);
+                if(static::isUpdater($display)){
+                    continue;
+                }
+                
                 if (!is_dir($srcFile)) {
-                    if(rename($srcFile, $dstFile)){
-                        echo "Successfully moved: " . static::displayPath($dstFile) . "\n";
+                    echo "Analyzing  $display ................ ";
+                    if (!file_exists($dstFile) || static::fileChanged($srcFile, $dstFile)) {
+                        if(rename($srcFile, $dstFile)){
+                            echo "DONE\n";
+                        }else{
+                            echo "FAILED\n";
+                        }
                     }else{
-                        echo "Unable to move: " . static::displayPath($dstFile) . "\n";
+                        echo "SKIPPED\n";
                     }
                 } else {
-                    static::checkAndMoveDirectory($dstFile, $srcFile); 
+                    static::checkAndMoveFolderRecursive($dstFile, $srcFile); 
                 }
             }
         }
-
-       // rmdir($source);
-        echo "Removed directory: $source\n";
     }
 
     private static function checkAndCopyDirectory(string $destination, string $source): void
@@ -182,8 +199,6 @@ class Updater{
         }
     }    
 
-    
-
     private static function displayPath(string $path): string 
     {
         return dirname($path) . '/' . basename($path);
@@ -197,8 +212,12 @@ class Updater{
         }
     }
 
-    //"nanoblocktech/psr-cache": "^1.0",
-    private static function moveFrameworkDirectory(string $destination, string $source,  string $codes): void
+    private static function fileChanged(string $srcFile, string $dstFile): bool
+    {
+        return md5_file($srcFile) !== md5_file($dstFile);
+    }
+
+    private static function onInstallAndUpdateFramework(string $destination, string $source,  string $codes): void
     {
         $fullSource = $source . $codes;
         if(file_exists($fullSource)){
@@ -208,64 +227,43 @@ class Updater{
                 if ($file !== '.' && $file !== '..') {
                     $srcFile = rtrim($fullSource, '/') . "/$file";
                     $dstFile = rtrim($destination, '/') . "/$file";
-                    
-                    if (!is_dir($srcFile)) {
-                        if(file_exists($dstFile)){
-                            echo "Removing old version of: " . static::displayPath($dstFile) . "\n";
-                            unlink($dstFile);
-                        }
+                    $display = static::displayPath($dstFile);
 
-                        if (copy($srcFile, $dstFile)) {
-                            echo "Copying " . static::displayPath($dstFile) . " ...\n";
-                            if(unlink($srcFile)){
-                                echo "Copy completed: " . static::displayPath($dstFile) . "\n";
+    
+                    if(static::isUpdater($display)){
+                        continue;
+                    }
+
+                    if (!is_dir($srcFile)) {
+                        echo "Analyzing  $display ................ ";
+                        if (!file_exists($dstFile) || static::fileChanged($srcFile, $dstFile)) {
+                            if(file_exists($dstFile)){
+                                unlink($dstFile);
                             }
-                        } else {
-                            echo "Failed to copy: " . static::displayPath($srcFile) . "\n";
+
+                            if (copy($srcFile, $dstFile)) {
+                                unlink($srcFile);
+                                echo "DONE\n";
+                            } else {
+                                echo "FAILED\n";
+                            }
+                        }else{
+                            echo "SKIPPED\n";
                         }
                     } else if (is_dir($srcFile)) {
-                        static::checkAndMoveDirectory($dstFile, $srcFile);
+                        static::checkAndMoveFolderRecursive($dstFile, $srcFile);
                     }
                 }
             }
 
-           /* echo "Removing  psr-cache\n";
-            exec('LM_DEBUG_MODE=1 composer remove nanoblocktech/psr-cache --no-update', $output, $returnCode);
-
-            $composerJsonPath = dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'composer.json';
-            $composer = file_get_contents($composerJsonPath);
-
-            if ($composer !== false) {
-                $composer = json_decode($composer, true);
-
-                if (isset($composer['require']['nanoblocktech/psr-cache'])) {
-                    echo "Updating Composer.json\n";
-                    unset($composer['require']['nanoblocktech/psr-cache']);
-                    $jsonString = json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-                    if ($jsonString !== false) {
-                        file_put_contents($composerJsonPath, $jsonString);
-                    } else {
-                        echo "Error encoding JSON.\n";
-                    }
-                }
-            }
-
-            // After copying all files, delete the original directory
-            //self::removeRecursive($source);
-          
-            echo "Cleaning and updating framework dependency...:\n";
+            self::removeRecursive(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . $source, 'framework');
+            exec('LM_DEBUG_MODE=1 composer dumpautoload', $output, $returnCode);
             foreach ($output as $line) {
-                echo $line . "\n";
+                echo  "Executing: $line\n";
             }
-        
             if ($returnCode === 0) {
-                echo "Updating dumpautoload\n";
-                exec('LM_DEBUG_MODE=1 composer dumpautoload');
-                self::removeRecursive(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . $source);
                 echo "Framework updated: $source\n";
-            }*/
-            self::removeRecursive(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . $source, 'psr-cache');
-            exec('composer dumpautoload');
+            }
         }
     }
 }
