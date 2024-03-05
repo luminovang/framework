c773c5c256c1974202d839d49226e5fdd152f470
Chigozie Peter <ujahchigozie@gmail.com>
2024-03-06 00:12:39 +0800

    Delete Http directory

--- Http/AsyncClientInterface.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace Luminova\Http;
-
-use GuzzleHttp\Promise\PromiseInterface;
-use GuzzleHttp\Psr7\Request as GuzzleRequest;
-interface AsyncClientInterface
-{
-    public function sendAsync(GuzzleRequest $request): PromiseInterface;
-}
