ca66ce6127df8d1a09d5eb6965bb965cd3422d10
Peter BBT <ujahchigozie@gmail.com>
2024-03-16 22:56:32 +0800

    New update and changes

--- src/Config/Configuration.php
+++ src/Config/Configuration.php
@@ -14,12 +14,12 @@ abstract class Configuration
     /**
     * @var string $version version name
     */
-    public static $version = '2.4.0';
+    public static $version = '2.5.0';
 
     /**
     * @var int $versionCode version code
     */
-    public static $versionCode = 240;
+    public static $versionCode = 250;
 
     /**
      * Minimum required php version
