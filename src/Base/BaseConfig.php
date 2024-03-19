e9a77fb3330a67250f03baf774085aab02a589b1
Peter BBT <ujahchigozie@gmail.com>
2024-03-19 21:34:15 +0800

    New update and changes

--- src/Base/BaseConfig.php
+++ src/Base/BaseConfig.php
@@ -15,12 +15,12 @@ abstract class BaseConfig
     /**
     * @var string $version version name
     */
-    public static $version = '2.8.0';
+    public static $version = '2.9.0';
 
     /**
     * @var int $versionCode version code
     */
-    public static $versionCode = 280;
+    public static $versionCode = 290;
 
     /**
      * Minimum required php version
