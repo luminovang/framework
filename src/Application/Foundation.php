1951c0f374d15547e1aba55d5e47a54ab762fc5b
Peter BBT <ujahchigozie@gmail.com>
2024-11-25 22:40:38 +0800

    New update and changes

--- src/Application/Foundation.php
+++ src/Application/Foundation.php
@@ -19,7 +19,7 @@ final class Foundation
      * 
      * @var string VERSION
      */
-    public const VERSION = '3.3.8';
+    public const VERSION = '3.3.9';
 
     /**
      * Framework version name.
